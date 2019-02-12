83797ee4b9529b85a666a47bc7740b6ed6eaa0cc
bernyfrancois <bernystone2k@gmail.com>
2019-02-12 15:32:13 +0100

    Finaly make the contact gestion.. of Open classroom

--- testjs/activiteOC/js/contacts.js
+++ testjs/activiteOC/js/contacts.js
@@ -1,25 +1,51 @@
 /*
 Activité : gestion des contacts
 */
-class Contact {
+class Contact {//classe pour creer un contact
   constructor(prenom, nom, numero) {
-    this.prenom: prenom;
-    this.nom: nom;
-    this.numero: numero;
+    this.prenom = prenom;
+    this.nom = nom;
+    this.numero = numero;
   }
-  decrire(){
-    console.log(`Prenom: ${this.prenom}, Nom: ${this.nom}, Numero`)
+  decrire(){//methode pour la description
+    console.log(`Prenom: ${this.prenom}\nNom: ${this.nom}\nNumero: ${this.numero}`)
   }
+}
+// TODO : complétez le programme
+function lister(){//lister les contacts
+  for(let i = 0; i < contacts.length; i++){
+    console.log(`Contact ${i+1}`)
+    contacts[i].decrire();
+  }
+}
+function ajouter(){//ajouter un contacte
+  let prenom = prompt('Entrez le prenom :');
+  let nom = prompt('Entrez le nom :');
+  let numero = prompt('Entrez le numero :');
+  let contact3 = new Contact(prenom, nom, numero);
+  contacts.push(contact3);
+  console.log('votre contact a été ajouté');
 }
 let contact = new Contact('Carole','Lévisse', '04594130' );
 let contact1 = new Contact('Mélodie','Nelsonne', '04596190' );
-// TODO : complétez le programme
+//tableau contenant tout les contacts
+const contacts = [contact, contact1];
 console.log("Bienvenue dans le gestionnaire de contacts !");
+lister();
 console.log("1: Lister les contacts\n2: Ajouter un contacts\n3: Quitter");
-let option = prompt('Choisissez une option');
-while (option !== "3") {//tan que l'user n'a pas choisis l'option Quitter
- if(option = "1"){
-   listContacts();//lister les contacts
- }
-}
-console.log('Bye, Bye');
+let option = '3';
+do{
+   option = prompt("Choisissez une option");
+  switch (option) {
+    case '1':
+      lister();
+      break;
+    case '2':
+      ajouter();
+      break;
+    default:
+    console.log(`option inconnu`);
+  }
+} while (option != '3');
+
+console.log('Aurevoir.')
