46218b9378b108f0aed008aae4d8a44ee874673e
bernyfrancois <bernystone2k@gmail.com>
2019-02-18 14:18:44 +0100

    correct error to  s6 file to sprint folder

--- sprint/s6.js
+++ sprint/s6.js
@@ -1,6 +1,6 @@
 //S6 Majeur ou Mineur?
 let age = Number(window.prompt('Vous avez que age ?'));
-if(age > 18){
+if(age => 18){
   console.log('Vous etes majeur');
 } else{
   console.log('Vous etes Mineur');
