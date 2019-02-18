910035fbfa73feede7a093ceed4cfa193aec8359
bernyfrancois <bernystone2k@gmail.com>
2019-02-18 14:46:08 +0100

    correct error to  s5 file to sprint folder

--- sprint/s5.js
+++ sprint/s5.js
@@ -3,10 +3,8 @@ let nbre1 = Number(window.prompt('Entrez le premier nombre'));
 let nbre2 = Number(window.prompt('Entrez le deuxieme nombre'));
 if(nbre1 > nbre2){
   console.log(`${nbre1} est le plus grand`);
-} else{
-  if(nbre1 == nbre2){
-    console.log("Ces nombres sont égaux, tu te moque de moi?")
-  } else {
-    console.log(`${nbre2} est le plus grand`)
-  }
+}else if(nbre1 < nbre2){
+  console.log(`${nbre2} est le plus grand`)
+} else {
+  console.log("Ces nombres sont égaux, tu te moque de moi?")
 }
