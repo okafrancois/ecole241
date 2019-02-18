69e91770b9031c159b67f4a9145cdcb4ee809c4f
bernyfrancois <bernystone2k@gmail.com>
2019-02-18 13:35:52 +0100

    add some condition  to s3 file of sprint folder

--- sprint/s3.js
+++ sprint/s3.js
@@ -3,5 +3,9 @@ let nombre = 5;
 if(nombre  > 10){
   console.log('Le nombre est superieur à 10');
 } else {
-  console.log('Le nombre est inferieur à 10');
+  if(nombre === 10){
+    console.log('Le nombre est égal à 10')
+  } else{
+    console.log('Le nombre est inferieur à 10');
+  }
 }
