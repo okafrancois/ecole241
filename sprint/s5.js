5fdfaf1c5f21d6bee287bb9039cb6b61e064f8a6
bernyfrancois <bernystone2k@gmail.com>
2019-02-18 14:11:15 +0100

    correct error to  s5 file to sprint folder

--- sprint/s5.js
+++ sprint/s5.js
@@ -1,6 +1,6 @@
 //S5 programme qui affiche le plus grand entre deux nombre
-let nbre1 = window.prompt('Entrez le premier nombre');
-let nbre2 = window.prompt('Entrez le deuxieme nombre');
+let nbre1 = Number(window.prompt('Entrez le premier nombre'));
+let nbre2 = Number(window.prompt('Entrez le deuxieme nombre'));
 if(nbre1 > nbre2){
   console.log(`${nbre1} est le plus grand`);
 } else{
