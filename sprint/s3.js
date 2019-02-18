4b609b3fb37c185842817f7b00d5c38a6e7494eb
bernyfrancois <bernystone2k@gmail.com>
2019-02-18 13:11:41 +0100

    add some text to s3 file of sprint folder

--- sprint/s3.js
+++ sprint/s3.js
@@ -1,7 +1,7 @@
 //S3 superieur ou inferieur a 10?
 let nombre = "5";
 if(nombre  > 10){
-  console.log('Superieur');
+  console.log('Le nombre est superieur à 10');
 } else {
-  console.log('inferieur');
+  console.log('Le nombre est inferieur à 10');
 }
