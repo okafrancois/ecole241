4a4ef9315e5a0626cd4e382a64aba8015e5c1829
francois <bernyfrancois>
2019-03-26 11:48:42 +0100

    ajout de la structure html dans le fichier act1.php du dossier php dans sprint

--- sprint/php/act1.php
+++ sprint/php/act1.php
@@ -1,1 +1,10 @@
-<?php echo "OKaCode !"; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Act 1</title>
+  </head>
+  <body>
+    <?php echo "OkaCode !"; ?>
+  </body>
+</html>
