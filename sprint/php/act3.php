ad13bd6b3bda7d4fa097bf95f00d30f8d497cfac
francois <bernyfrancois>
2019-03-27 10:14:45 +0100

    ajout du code php dans le fichier act3.php du dossier php dans sprint

---
+++ sprint/php/act3.php
@@ -0,0 +1,18 @@
+<?php
+// Des prenoms dans un tableau php
+//tableau associatif
+$prenoms = array(
+  1 => "Berny",
+  2 => "Stone",
+  3 => "François",
+  4 => "Disick",
+  5 => "Henry",
+  6 => "Junior",
+  7 => "Red",
+  8 => "John",
+  9 => "Noel",
+  10 => "Prenom",
+ );
+ // tableau simple
+$prenoms2 = array("Berny", "Stone", "François", "Disick", "Henry", "Junior", "Red", "John", "Noel", "Prenom");
+?>
