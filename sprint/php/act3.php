6c651c002636e87e5042b46c6eed1e2de7fea698
francois <bernyfrancois>
2019-03-27 10:46:36 +0100

    ajout du code php dans le fichier act3.php du dossier php dans sprint

--- sprint/php/act3.php
+++ sprint/php/act3.php
@@ -15,4 +15,13 @@ $prenoms = array(
  );
  // tableau simple
 $prenoms2 = array("Berny", "Stone", "François", "Disick", "Henry", "Junior", "Red", "John", "Noel", "Prenom");
+// Parcours du premier tableau
+for($i = 1; $i <= 10; $i++){
+  echo "$i : $prenoms[$i] <br>";
+};
+echo "<br>";
+// parcours du deuxieme tableau
+foreach($prenoms as $prenom){
+  echo "<li>$prenom</li>";
+};
 ?>
