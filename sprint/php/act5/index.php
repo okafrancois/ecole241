ae7d48637eb1dcd45ada9b3358fcc16aee690418
bernyfrancois <bernystone2k@gmail.com>
2019-04-01 16:30:16 +0100

    act5 folder add

--- sprint/php/act5/index.php
+++ sprint/php/act5/index.php
@@ -7,9 +7,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>ecole{241}</title>
-
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="cover.css" rel="stylesheet">
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -30,51 +29,48 @@
       }
     </style>
     <!-- Custom styles for this template -->
-    <link href="cover.css" rel="stylesheet">
   </head>
-  <body class="text-center">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header class="masthead mb-auto">
-    <div class="inner">
-      <h3 class="masthead-brand">ecole{241}</h3>
-      <nav class="nav nav-masthead justify-content-center">
-        <a class="nav-link active" href="#">Accueil</a>
-      </nav>
-    </div>
-  </header>
-  <br><br>
-  <main role="main" class="inner cover">
-    <table class="table table-hover table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Prenom</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Age</th>
-      <th scope="col">Profil</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-      $i = 1;
-      foreach ($liste as $el) :?>
-      <tr>
-        <td><?= $i ?></td>
-        <td><?= $el['prenom'] ?></td>
-        <td><?= $el['nom'] ?></td>
-        <td><?php if ($el['age']==="NaN") {
-          echo "inconnu";
-        } else {
-          echo "{$el['age']} ans";
-        }?></td>
-        <td><a href="<?= $el['profil']?>" target="_blank">Consulter</a> </td>
-      </tr>
-      <?php $i++ ?>
-    <?php endforeach; ?>
-  </tbody>
- </table>
-  </main>
-
+    <body class="text-center">
+      <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="masthead mb-auto">
+          <div class="inner">
+            <h3 class="masthead-brand">ecole{241}</h3>
+            <nav class="nav nav-masthead justify-content-center">
+              <a class="nav-link active" href="#">Accueil</a>
+            </nav>
+          </div>
+        </header>
+        <br><br>
+        <main role="main" class="inner cover">
+          <table class="table table-hover table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Prenom</th>
+              <th scope="col">Nom</th>
+              <th scope="col">Age</th>
+              <th scope="col">Profil</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              $i = 1;
+              foreach ($liste as $key => $el) :?>
+              <tr>
+                <td><?= $key+1 ?></td>
+                <td><?= $el['prenom'] ?></td>
+                <td><?= $el['nom'] ?></td>
+                <td><?php if ($el['age']==="NaN") {
+                  echo "inconnu";
+                } else {
+                  echo "{$el['age']} ans";
+                }?></td>
+                <td><a href="<?= $el['profil']?>" target="_blank">Consulter</a> </td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+       </table>
+      </main>
   <footer class="mastfoot mt-auto">
     <div class="inner">
       <p>2019 à <a href="ecole241.org" target="_blank">ecole{241}</a> par Berny François</p>
