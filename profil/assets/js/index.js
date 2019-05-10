01976abde184624cbf0330da4de2c6921def98c0
francois <bernyfrancois>
2019-05-10 02:32:15 +0100

    replace profil by profilv2

--- profil/assets/js/index.js
+++ profil/assets/js/index.js
@@ -1,2 +1,6 @@
-console.log('hello');
-let deco = document.getElementsByClassName('deco');
+let skLevels = document.querySelectorAll('.skill-level');
+
+for (var i = 0; i < skLevels.length; i++) {
+  let value = skLevels[i].parentNode.parentNode.querySelector("span:last-of-type").textContent;
+  skLevels[i].style.width = value;
+}
--- profilv2/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-let skLevels = document.querySelectorAll('.skill-level');
-
-for (var i = 0; i < skLevels.length; i++) {
-  let value = skLevels[i].parentNode.parentNode.querySelector("span:last-of-type").textContent;
-  skLevels[i].style.width = value;
-}
