7d48a39bc6cde20fe170544022b608ea7f46ab0f
bernyfrancois <bernystone2k@gmail.com>
2019-06-14 08:58:27 +0200

    projet agri bourse

---
+++ AGRIBRS/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './Screens/Home'
+export default function App() {
+  return (
+    <Home />
+  );
+}
