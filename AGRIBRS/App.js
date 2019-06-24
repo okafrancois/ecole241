e5676deb77a5e23cf51471809b743970f654ca71
bernyfrancois <bernystone2k@gmail.com>
2019-06-24 09:49:45 +0100

    reorganisation du depot

--- AGRIBRS/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './Screens/Home'
-export default function App() {
-  return (
-    <Home />
-  );
-}
---
+++ js_snippets/api.js
@@ -0,0 +1,9 @@
+﻿var dataContainer = document.getElementById("dataContainer")
+ajaxGet("http://agribourse.alwaysdata.net/api/v1/home", function (reponse) {
+    // Transforme la réponse en un tableau d'articles
+    var datas = JSON.parse(reponse);
+    datas.data.forEach(function (data) {
+        // Ajout du titre et du contenu de chaque article
+
+    });
+});
